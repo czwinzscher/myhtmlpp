f3980687ed89f40c5c6fa95f2113acc9069d67b5
clemens <czwinzscher2@gmail.com>
2019-10-15 17:15:52 +0200

    initial commit

---
+++ include/parser.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <myhtml/api.h>
+#include <string>
+
+namespace myhtmlpp {
+
+class Parser {
+public:
+    explicit Parser(const std::string& html);
+    ~Parser();
+
+private:
+    myhtml_t* m_myhtml;
+    myhtml_tree_t* m_tree;
+};
+
+}  // namespace myhtmlpp
