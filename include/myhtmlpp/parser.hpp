78974ec052cfbdd5c5ed1922cd2cf5fd083d955a
clemens <czwinzscher2@gmail.com>
2019-10-19 16:25:39 +0200

    add functions to get collections of nodes from Parser

--- include/myhtmlpp/parser.hpp
+++ include/myhtmlpp/parser.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "collection.hpp"
 #include "node.hpp"
 
 #include <myhtml/api.h>
@@ -12,7 +13,16 @@ public:
     explicit Parser(const std::string& html);
     ~Parser();
 
-    myhtmlpp::Node root();
+    Node root();
+
+    Collection nodes_by_tag_id(myhtml_tag_id_t tag_id);
+    Collection nodes_by_name(const std::string& name);
+    Collection nodes_by_attribute_key(const std::string& key);
+    Collection nodes_by_attribute_value(const std::string& value,
+                                        bool case_insensitive = false);
+    Collection nodes_by_attribute_value_with_key(const std::string& key,
+                                                 const std::string& value,
+                                                 bool case_insensitive = false);
 
 private:
     myhtml_t* m_raw_myhtml;
--- src/parser.cpp
+++ src/parser.cpp
@@ -17,5 +17,47 @@ myhtmlpp::Parser::~Parser() {
 }
 
 myhtmlpp::Node myhtmlpp::Parser::root() {
-    return myhtmlpp::Node(myhtml_tree_get_document(m_raw_tree));
+    return Node(myhtml_tree_get_document(m_raw_tree));
+}
+
+myhtmlpp::Collection myhtmlpp::Parser::nodes_by_tag_id(myhtml_tag_id_t tag_id) {
+    myhtml_collection_t* c =
+        myhtml_get_nodes_by_tag_id(m_raw_tree, nullptr, tag_id, nullptr);
+
+    return Collection(c);
+}
+
+myhtmlpp::Collection myhtmlpp::Parser::nodes_by_name(const std::string& name) {
+    myhtml_collection_t* c = myhtml_get_nodes_by_name(
+        m_raw_tree, nullptr, name.c_str(), strlen(name.c_str()), nullptr);
+
+    return Collection(c);
+}
+
+myhtmlpp::Collection
+myhtmlpp::Parser::nodes_by_attribute_key(const std::string& key) {
+    myhtml_collection_t* c = myhtml_get_nodes_by_attribute_key(
+        m_raw_tree, nullptr, nullptr, key.c_str(), strlen(key.c_str()),
+        nullptr);
+
+    return Collection(c);
+}
+
+myhtmlpp::Collection
+myhtmlpp::Parser::nodes_by_attribute_value(const std::string& value,
+                                           bool case_insensitive) {
+    myhtml_collection_t* c = myhtml_get_nodes_by_attribute_value(
+        m_raw_tree, nullptr, nullptr, case_insensitive, nullptr, 0,
+        value.c_str(), strlen(value.c_str()), nullptr);
+
+    return Collection(c);
+}
+
+myhtmlpp::Collection myhtmlpp::Parser::nodes_by_attribute_value_with_key(
+    const std::string& key, const std::string& value, bool case_insensitive) {
+    myhtml_collection_t* c = myhtml_get_nodes_by_attribute_value(
+        m_raw_tree, nullptr, nullptr, case_insensitive, key.c_str(),
+        strlen(key.c_str()), value.c_str(), strlen(value.c_str()), nullptr);
+
+    return Collection(c);
 }
