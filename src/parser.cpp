be13e2deb0edf9c36c5e26b30d170d15aa31c60e
clemens <czwinzscher2@gmail.com>
2019-10-26 23:45:22 +0200

    add methods to parse fragments

--- src/parser.cpp
+++ src/parser.cpp
@@ -42,6 +42,40 @@ myhtmlpp::Tree myhtmlpp::Parser::parse_with_options(const std::string& html,
     return Tree(raw_tree);
 }
 
+myhtmlpp::Tree myhtmlpp::Parser::parse_fragment(const std::string& html,
+                                                myhtml_tag_id_t tag_id,
+                                                myhtml_namespace ns) {
+    reset();
+
+    m_raw_myhtml = myhtml_create();
+    myhtml_init(m_raw_myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
+
+    myhtml_tree_t* raw_tree = myhtml_tree_create();
+    myhtml_tree_init(raw_tree, m_raw_myhtml);
+
+    myhtml_parse_fragment(raw_tree, MyENCODING_UTF_8, html.c_str(),
+                          strlen(html.c_str()), tag_id, ns);
+
+    return Tree(raw_tree);
+}
+
+myhtmlpp::Tree myhtmlpp::Parser::parse_fragment_with_options(
+    const std::string& html, myhtml_tag_id_t tag_id, myhtml_namespace ns,
+    myhtml_options opt, size_t thread_count, size_t queue_size) {
+    reset();
+
+    m_raw_myhtml = myhtml_create();
+    myhtml_init(m_raw_myhtml, opt, thread_count, queue_size);
+
+    myhtml_tree_t* raw_tree = myhtml_tree_create();
+    myhtml_tree_init(raw_tree, m_raw_myhtml);
+
+    myhtml_parse_fragment(raw_tree, MyENCODING_UTF_8, html.c_str(),
+                          strlen(html.c_str()), tag_id, ns);
+
+    return Tree(raw_tree);
+}
+
 bool myhtmlpp::Parser::good() const { return m_raw_myhtml != nullptr; }
 
 void myhtmlpp::Parser::reset() {
