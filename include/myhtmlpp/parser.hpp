d7763777bef5b198fefee80694f0c0f1b1fe418d
clemens <czwinzscher2@gmail.com>
2019-10-30 22:45:41 +0100

    remove Parser class, use only functions

--- include/myhtmlpp/parser.hpp
+++ include/myhtmlpp/parser.hpp
@@ -9,27 +9,14 @@
 
 namespace myhtmlpp {
 
-class Parser {
-public:
-    Parser();
-    ~Parser();
-
-    [[nodiscard]] bool good() const;
-
-    Tree parse(const std::string& html);
-    Tree parse_with_options(const std::string& html, myhtml_options opt,
-                            size_t thread_count, size_t queue_size);
-    Tree parse_fragment(const std::string& html, myhtml_tag_id_t tag_id,
-                        myhtml_namespace ns);
-    Tree parse_fragment_with_options(const std::string& html,
-                                     myhtml_tag_id_t tag_id,
-                                     myhtml_namespace ns, myhtml_options opt,
-                                     size_t thread_count, size_t queue_size);
-
-private:
-    void reset();
-
-    myhtml_t* m_raw_myhtml;
-};
+Tree parse(const std::string& html);
+Tree parse_with_options(const std::string& html, myhtml_options opt,
+                        size_t thread_count, size_t queue_size);
+Tree parse_fragment(const std::string& html, myhtml_tag_id_t tag_id,
+                    myhtml_namespace ns);
+Tree parse_fragment_with_options(const std::string& html,
+                                 myhtml_tag_id_t tag_id, myhtml_namespace ns,
+                                 myhtml_options opt, size_t thread_count,
+                                 size_t queue_size);
 
 }  // namespace myhtmlpp
--- src/parser.cpp
+++ src/parser.cpp
@@ -5,82 +5,61 @@
 #include <myhtml/api.h>
 #include <string>
 
-myhtmlpp::Parser::Parser() : m_raw_myhtml(nullptr) {}
-
-myhtmlpp::Parser::~Parser() { reset(); }
-
-myhtmlpp::Tree myhtmlpp::Parser::parse(const std::string& html) {
-    reset();
-
-    m_raw_myhtml = myhtml_create();
-    myhtml_init(m_raw_myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
+myhtmlpp::Tree myhtmlpp::parse(const std::string& html) {
+    myhtml_t* raw_myhtml = myhtml_create();
+    myhtml_init(raw_myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
 
     myhtml_tree_t* raw_tree = myhtml_tree_create();
-    myhtml_tree_init(raw_tree, m_raw_myhtml);
+    myhtml_tree_init(raw_tree, raw_myhtml);
 
     myhtml_parse(raw_tree, MyENCODING_UTF_8, html.c_str(),
                  strlen(html.c_str()));
 
-    return Tree(raw_tree);
+    return Tree(raw_myhtml, raw_tree);
 }
 
-myhtmlpp::Tree myhtmlpp::Parser::parse_with_options(const std::string& html,
-                                                    myhtml_options opt,
-                                                    size_t thread_count,
-                                                    size_t queue_size) {
-    reset();
-
-    m_raw_myhtml = myhtml_create();
-    myhtml_init(m_raw_myhtml, opt, thread_count, queue_size);
+myhtmlpp::Tree myhtmlpp::parse_with_options(const std::string& html,
+                                            myhtml_options opt,
+                                            size_t thread_count,
+                                            size_t queue_size) {
+    myhtml_t* raw_myhtml = myhtml_create();
+    myhtml_init(raw_myhtml, opt, thread_count, queue_size);
 
     myhtml_tree_t* raw_tree = myhtml_tree_create();
-    myhtml_tree_init(raw_tree, m_raw_myhtml);
+    myhtml_tree_init(raw_tree, raw_myhtml);
 
     myhtml_parse(raw_tree, MyENCODING_UTF_8, html.c_str(),
                  strlen(html.c_str()));
 
-    return Tree(raw_tree);
+    return Tree(raw_myhtml, raw_tree);
 }
 
-myhtmlpp::Tree myhtmlpp::Parser::parse_fragment(const std::string& html,
-                                                myhtml_tag_id_t tag_id,
-                                                myhtml_namespace ns) {
-    reset();
-
-    m_raw_myhtml = myhtml_create();
-    myhtml_init(m_raw_myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
+myhtmlpp::Tree myhtmlpp::parse_fragment(const std::string& html,
+                                        myhtml_tag_id_t tag_id,
+                                        myhtml_namespace ns) {
+    myhtml_t* raw_myhtml = myhtml_create();
+    myhtml_init(raw_myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
 
     myhtml_tree_t* raw_tree = myhtml_tree_create();
-    myhtml_tree_init(raw_tree, m_raw_myhtml);
+    myhtml_tree_init(raw_tree, raw_myhtml);
 
     myhtml_parse_fragment(raw_tree, MyENCODING_UTF_8, html.c_str(),
                           strlen(html.c_str()), tag_id, ns);
 
-    return Tree(raw_tree);
+    return Tree(raw_myhtml, raw_tree);
 }
 
-myhtmlpp::Tree myhtmlpp::Parser::parse_fragment_with_options(
+myhtmlpp::Tree myhtmlpp::parse_fragment_with_options(
     const std::string& html, myhtml_tag_id_t tag_id, myhtml_namespace ns,
     myhtml_options opt, size_t thread_count, size_t queue_size) {
-    reset();
-
-    m_raw_myhtml = myhtml_create();
-    myhtml_init(m_raw_myhtml, opt, thread_count, queue_size);
+    myhtml_t* raw_myhtml = myhtml_create();
+    myhtml_init(raw_myhtml, opt, thread_count, queue_size);
 
     myhtml_tree_t* raw_tree = myhtml_tree_create();
-    myhtml_tree_init(raw_tree, m_raw_myhtml);
+    myhtml_tree_init(raw_tree, raw_myhtml);
 
     myhtml_parse_fragment(raw_tree, MyENCODING_UTF_8, html.c_str(),
                           strlen(html.c_str()), tag_id, ns);
 
-    return Tree(raw_tree);
-}
-
-bool myhtmlpp::Parser::good() const { return m_raw_myhtml != nullptr; }
-
-void myhtmlpp::Parser::reset() {
-    if (m_raw_myhtml != nullptr) {
-        myhtml_destroy(m_raw_myhtml);
-        m_raw_myhtml = nullptr;
-    }
+    return Tree(raw_myhtml, raw_tree);
 }
