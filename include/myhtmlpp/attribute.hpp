61b65d359b0c67046bf4c2f1b6832e787d48fd62
clemens <czwinzscher2@gmail.com>
2019-11-04 01:04:04 +0100

    add structured bindings for attributes

--- include/myhtmlpp/attribute.hpp
+++ include/myhtmlpp/attribute.hpp
@@ -22,8 +22,31 @@ public:
     [[nodiscard]] std::optional<Attribute> previous() const;
     [[nodiscard]] std::optional<Attribute> next() const;
 
+    // structured bindings
+    // auto [key, value] = attr;
+    template <std::size_t N>
+    decltype(auto) get() const {
+        if constexpr (N == 0) {
+            return key();
+        } else if constexpr (N == 1) {
+            return value();
+        }
+    }
+
 private:
     myhtml_tree_attr_t* m_raw_attribute;
 };
 
 }  // namespace myhtmlpp
+
+namespace std {
+template <>
+struct tuple_size<myhtmlpp::Attribute>
+    : std::integral_constant<std::size_t, 2> {};
+
+template <std::size_t N>
+struct tuple_element<N, myhtmlpp::Attribute> {
+    using type = decltype(std::declval<myhtmlpp::Attribute>().get<N>());
+};
+
+}  // namespace std
