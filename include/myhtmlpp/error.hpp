7ac967a85b854bd6e0827797a216588fc9040aae
clemens <czwinzscher2@gmail.com>
2019-11-08 18:24:50 +0100

    add some basic documentation

--- include/myhtmlpp/error.hpp
+++ include/myhtmlpp/error.hpp
@@ -6,16 +6,19 @@
 
 namespace myhtmlpp {
 
+/// Exception indicating that `myhtml_init` failed.
 class init_error : public std::runtime_error {
 public:
     explicit init_error(mystatus_t status);
 };
 
+/// Exception indicating that `myhtml_tree_init` failed.
 class tree_init_error : public std::runtime_error {
 public:
     explicit tree_init_error(mystatus_t status);
 };
 
+/// Exception indicating that `myhtml_parse` or `myhtml_parse_fragment` failed.
 class parse_error : public std::runtime_error {
 public:
     explicit parse_error(mystatus_t status);
