47fa8ff343ecc4cce4ae441a3bea9bda6afb26b9
clemens <czwinzscher2@gmail.com>
2019-11-09 20:20:30 +0100

    return nullopt if argument is nullptr

--- src/utils.hpp
+++ src/utils.hpp
@@ -4,6 +4,10 @@
 
 template <typename Ret, typename Func, typename Arg>
 std::optional<Ret> optional_helper(Func f, Arg a) {
+    if (a == nullptr) {
+        return std::nullopt;
+    }
+
     auto raw = f(a);
 
     return raw != nullptr ? std::make_optional(Ret(raw)) : std::nullopt;
