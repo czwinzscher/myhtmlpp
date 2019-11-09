805a8c82e2f40f5cbd0adeb23d2b20b7652d27d1
clemens <czwinzscher2@gmail.com>
2019-11-09 23:32:52 +0100

    create an exception base class that holds the myhtml_status

--- include/myhtmlpp/error.hpp
+++ include/myhtmlpp/error.hpp
@@ -1,24 +1,40 @@
 #pragma once
 
+#include <exception>
 #include <mycore/myosi.h>
 #include <stdexcept>
 
 namespace myhtmlpp {
 
+/// A general exception bas class.
+class myhtml_error : public std::exception {
+public:
+    [[nodiscard]] const char* what() const noexcept override;
+
+    [[nodiscard]] mystatus_t status_code() const;
+
+protected:
+    myhtml_error(mystatus_t status_code, const char* what);
+
+private:
+    mystatus_t m_status;
+    std::runtime_error m_error;
+};
+
 /// Exception indicating that `myhtml_init` failed.
-class init_error : public std::runtime_error {
+class init_error : public myhtml_error {
 public:
     explicit init_error(mystatus_t status);
 };
 
 /// Exception indicating that `myhtml_tree_init` failed.
-class tree_init_error : public std::runtime_error {
+class tree_init_error : public myhtml_error {
 public:
     explicit tree_init_error(mystatus_t status);
 };
 
 /// Exception indicating that `myhtml_parse` or `myhtml_parse_fragment` failed.
-class parse_error : public std::runtime_error {
+class parse_error : public myhtml_error {
 public:
     explicit parse_error(mystatus_t status);
 };
--- src/error.cpp
+++ src/error.cpp
@@ -1,15 +1,28 @@
 #include "myhtmlpp/error.hpp"
 
+#include <mycore/myosi.h>
 #include <string>
 
+myhtmlpp::myhtml_error::myhtml_error(mystatus_t status, const char* what)
+    : m_status(status), m_error(what) {}
+
+const char* myhtmlpp::myhtml_error::what() const noexcept {
+    return m_error.what();
+}
+
+mystatus_t myhtmlpp::myhtml_error::status_code() const { return m_status; }
+
 myhtmlpp::init_error::init_error(mystatus_t status)
-    : std::runtime_error("myhtml_init failed with status " +
-                         std::to_string(status)) {}
+    : myhtml_error(status,
+                   ("myhtml_init failed with status " + std::to_string(status))
+                       .c_str()) {}
 
 myhtmlpp::tree_init_error::tree_init_error(mystatus_t status)
-    : std::runtime_error("myhtml_tree_init failed with status " +
-                         std::to_string(status)) {}
+    : myhtml_error(status, ("myhtml_tree_init failed with status " +
+                            std::to_string(status))
+                               .c_str()) {}
 
 myhtmlpp::parse_error::parse_error(mystatus_t status)
-    : std::runtime_error("parsing failed with status " +
-                         std::to_string(status)) {}
+    : myhtml_error(
+          status,
+          ("parsing failed with status " + std::to_string(status)).c_str()) {}
