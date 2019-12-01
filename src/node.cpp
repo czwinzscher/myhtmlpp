39c31c47e21cabb2c2d0433dabd81dcb90ce64d8
clemens <czwinzscher2@gmail.com>
2019-12-01 18:08:49 +0100

    add template parameter explicitely

--- src/node.cpp
+++ src/node.cpp
@@ -202,7 +202,7 @@ std::optional<myhtmlpp::Attribute> myhtmlpp::Node::last_attribute() const {
 }
 
 std::vector<myhtmlpp::Attribute> myhtmlpp::Node::attributes() const {
-    return std::vector(begin(), end());
+    return std::vector<Attribute>(begin(), end());
 }
 
 // Iterator
